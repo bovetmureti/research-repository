fcb88684c810a0392f25b5dc6fde36c3104ef6b9
Christopher Rateng <44862569+ItsKris62@users.noreply.github.com>
2025-03-14 10:59:19 +0000

    made wording changes

--- acwict-clone/src/components/Hero.jsx
+++ acwict-clone/src/components/Hero.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
       <div className="hero">
         <div className="hero-overlay">
           <div className="container hero-content">
-            <h1>Empowering Women Through Technology</h1>
-            <p>African Center for Women, Information and Communications Technology</p>
+            <h1>KICTANET Empowering Through Technology</h1>
+            <p>KICTANET, Information and Communications Technology</p>
             <div className="hero-buttons">
               <a href="#programs" className="btn">Our Programs</a>
               <a href="#donate" className="btn btn-outline">Support Us</a>
